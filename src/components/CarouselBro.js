d1b04fe9c099afe13a3e86f5ca2288527c6887e5
acong <acong@example.com>
2018-10-03 11:57:09 +0700

    savepoint

--- src/components/CarouselBro.js
+++ src/components/CarouselBro.js
@@ -2,41 +2,46 @@ import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from 'react-router-dom';
 import XPS15 from '../supports/img/XPS 15 - 1.jpg';
+import Cookies from 'universal-cookie';
+import Axios from 'axios';
+
+const cookies = new Cookies();
 
 class CarouselBro extends Component {
+
+    state = { products: [] }
+
+    selectedProduct(id){
+        cookies.set('SelectedProduct', id, { path: '/' });
+    }
+    
+    renderCaroussel=()=>{
+        var caroussel = []
+        Axios.get("http://localhost:1002/carousselProducts")
+        .then(ok=>{
+            console.log(ok.data)
+            caroussel.push(ok.data);
+        })    
+        console.log(caroussel)
+        return caroussel.map(data=>{
+            console.log(data.Img)
+            return(
+                <div className='geser'>
+                    <Link to="/productDetails" onClick={ () => this.selectedProduct(data.idproducts)}>
+                        <div>
+                            <img src={data.Img} alt=''/>
+                        </div>
+                    </Link>
+                </div>
+            )
+        })
+    }
+
     render() {
         return(
             <div className='perlebar '>
-                {/* <h3>
-                    Recommended Items
-                </h3> */}
-                <Carousel className=' carousselAdjuster timpaCSScaroussel' infiniteLoop={true} showThumbs={false} autoPlay={true}>
-
-                    <div className='geser'>
-                        <Link to="/productDetails">
-                            <div>
-                                <img src={XPS15} alt=''/>
-                            </div>
-                        </Link>
-                    {/* <p className="legend">{this.props.legend2}</p> */}
-                    </div>
-                
-                    <div className='geser'>
-                        <Link to="/productDetails">
-                            <div>
-                                <img src="http://www.itguyswa.com.au/wp-content/uploads/2017/06/laptop-xps-15-9550-pdp-polaris-02.jpg" alt=''/>
-                            </div>
-                        </Link>
-                    </div>
-                    
-                    <div className='geser'>
-                        <Link to="/productDetails1">
-                            <div>
-                                <img src="https://images.indianexpress.com/2017/06/xiaomi-mi-notebook-air-13-official.jpg" alt=''/>
-                            </div>
-                        </Link>
-                    </div>
-                    
+                <Carousel className=' carousselAdjuster timpaCSScaroussel'  showThumbs={false} autoPlay={true}>
+                    {/* {this.renderCaroussel()} */}
                 </Carousel>
                 <br/>
             </div>
