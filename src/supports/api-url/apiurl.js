b0c9d63a99f7752ce31bd06c633e0ce806606b5c
acong <acong@example.com>
2018-10-10 07:36:17 +0700

    https

--- src/supports/api-url/apiurl.js
+++ src/supports/api-url/apiurl.js
@@ -1,2 +1,2 @@
 // export const API_URL_1 = "http://localhost:1002/";
-export const API_URL_1 = "http://portobackend.herokuapp.com/";
+export const API_URL_1 = "https://portobackend.herokuapp.com/";
