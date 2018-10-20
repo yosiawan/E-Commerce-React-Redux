a13e9dcc0c5a5efdb48b0505d5a53f1eaaebcde4
acong <acong@example.com>
2018-10-20 16:22:13 +0700

    converted HomePage into SFC

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,19 +3,13 @@ import CarouselBro from './CarouselBro';
 import ItemList from './homepageItemList';
 
 
-class HomePage extends Component {
-    
-        render() {
-            return (
-            <div style={{ marginTop: "50px" }} >
-                <CarouselBro/>
-                <ItemList/>
-                {/* <input style={{ marginBottom: "50px" }} type="button" value="Delete" onClick={this.onDeleteClick} className="btn btn-danger"/>
-                <input style={{ marginBottom: "50px" }} type="button" value="Edit" onClick={this.onEditClick} className="btn btn-warning"/> */}
-
-            </div>
-        );
-    }
+const HomePage = () => {
+    return (
+        <div style={{ marginTop: "50px" }} >
+            <CarouselBro/>
+            <ItemList/>
+        </div>
+    );
 }
 
 export default HomePage;
